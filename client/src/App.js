53bd40052a8e215b8630006bbec23983c6a21d96
Douglas Rosenberg <doug.rosenberg@gmail.com>
2021-05-20 22:13:53 -0500

    initial setup

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { GlobalStyle } from './globalStyles';
+
+function App() {
+  return (
+    <Router>
+      <GlobalStyle />
+      <Navbar />
+    </Router>
+  );
+}
+
+export default App;
